54785f8db384a4436576c6db76ae589de812e830
Chris Grayson <chris@activeprospect.com>
2018-05-04 10:54:28 -0500

    tweak css update script

--- update-client-css.sh
+++ update-client-css.sh
@@ -11,14 +11,33 @@ fi
 
 VERSION=$1
 
+CSS_DIR=lib/public
 NEW_CSS_FILE=lc-client.$VERSION.css
 
 URL=`echo $BASE_URL | sed "s/LC_CLIENT_SEMVER/$VERSION/"`
-curl -o $NEW_CSS_FILE $URL
+curl -o $CSS_DIR/$NEW_CSS_FILE $URL
 
+# if download succeeded
 if [ $? -eq 0 ]
 then
-	ln -sf $NEW_CSS_FILE lib/public/lc-client.css
+	echo -n "downloaded "
+	ls $NEW_CSS_FILE
+
+	# prompt to remove old versions
+	for f in $CSS_DIR/lc-client.*.css
+	do
+		if [ "$f" != "$CSS_DIR/$NEW_CSS_FILE" ]
+		then
+			rm -i "$f"
+			if [ ! -f "$CSS_DIR/$NEW_CSS_FILE" ]
+			then
+				git rm "$f"
+			fi
+		fi
+	done
+
+	# symlink new version
+	ln -sf $NEW_CSS_FILE $CSS_DIR/lc-client.css
 fi
 
-ls -lF lib/public/lc-client*.css
+ls -lF $CSS_DIR/lc-client*.css
