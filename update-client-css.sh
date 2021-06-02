19f4b99dcc719dbaf409a82a9ca1ccfe8f9cc881
Chris Grayson <cgrayson@airpost.net>
2021-06-02 11:46:24 -0500

    update dependencies & CSS

--- update-client-css.sh
+++ update-client-css.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 USAGE="$0: usage is: $0 lc-client-semver"
-BASE_URL="https://d35x2co4ayvx2v.cloudfront.net/build/LC_CLIENT_SEMVER/css/index.css"
 
 if [ $# -lt 1 ]
 then
@@ -12,35 +11,32 @@ fi
 VERSION=$1
 
 CSS_DIR=lib/public
-NEW_CSS_FILE=lc-client.$VERSION.css
+NEW_CSS_PATH=$CSS_DIR/lc-client.$VERSION.css
 
-URL=`echo $BASE_URL | sed "s/LC_CLIENT_SEMVER/$VERSION/"`
-curl -o $CSS_DIR/$NEW_CSS_FILE $URL
+URL="https://d35x2co4ayvx2v.cloudfront.net/build/${VERSION}/css/index.css"
+curl -s -o ${NEW_CSS_PATH}.gz $URL
 
 # if download succeeded
 if [ $? -eq 0 ]
 then
 	echo -n "downloaded "
-	ls $CSS_DIR/${NEW_CSS_FILE}.gz
+	ls ${NEW_CSS_PATH}.gz
 
-	gunzip -v $CSS_DIR/${NEW_CSS_FILE}.gz
+	gunzip -v ${NEW_CSS_PATH}.gz
 
 	# prompt to remove old versions
-	for f in $CSS_DIR/lc-client.*.css
+	for f in $CSS_DIR/lc-client.[0-9]*.css
 	do
-		if [ "$f" != "$CSS_DIR/$NEW_CSS_FILE" ]
+	  echo "processing $f..."
+		if [ "$f" != "$NEW_CSS_PATH" ]
 		then
-			rm -i "$f"
-			if [ ! -f "$f" ]
-			then
-				git rm "$f"
-			fi
+			rm -v "$f"
 		fi
 	done
 
 	# symlink new version
-	ln -sf $NEW_CSS_FILE $CSS_DIR/lc-client.css
-	git add $NEW_CSS_FILE $CSS_DIR/lc-client.css
+	ln -sf $NEW_CSS_PATH $CSS_DIR/lc-client.css
+	git add $NEW_CSS_PATH $CSS_DIR/lc-client.css
 fi
 
 ls -lF $CSS_DIR/lc-client*.css
